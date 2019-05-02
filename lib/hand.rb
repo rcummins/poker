3e317ba74512d96628b82ef39502f588a1b3569a
Renata Cummins <renatacummins@gmail.com>
2019-05-02 14:51:04 -0400

    Add Hand#classify_hand that can recognize everything except straights

--- lib/hand.rb
+++ lib/hand.rb
@@ -10,4 +10,45 @@ class Hand
         
         @cards << card
     end
+
+    def classify_hand
+        card_values = group_values
+        card_suits = group_suits
+
+        if card_values.values.sort == [1, 4]
+            :four_of_a_kind
+        elsif card_values.values.sort == [2, 3]
+            :full_house
+        elsif card_suits.values == [5]
+            :flush
+        elsif card_values.values.sort == [1, 1, 3]
+            :three_of_a_kind
+        elsif card_values.values.sort == [1, 2, 2]
+            :two_pairs
+        elsif card_values.values.sort == [1, 1, 1, 2]
+            :one_pair
+        end
+    end
+
+    private
+
+    def group_values
+        card_values = Hash.new(0)
+
+        @cards.each do |card|
+            card_values[card.value] += 1
+        end
+
+        return card_values
+    end
+
+    def group_suits
+        card_suits = Hash.new(0)
+
+        @cards.each do |card|
+            card_suits[card.suit] += 1
+        end
+
+        return card_suits
+    end
 end
--- spec/hand_spec.rb
+++ spec/hand_spec.rb
@@ -25,4 +25,84 @@ describe Hand do
             expect { hand.add_card(card) }.to raise_error(err_msg)
         end
     end
+
+    describe '#classify_hand' do
+        let(:card_JC)  { double('card', :value => 11, :suit => :clubs) }
+        let(:card_10C) { double('card', :value => 10, :suit => :clubs) }
+        let(:card_10D) { double('card', :value => 10, :suit => :diamonds) }
+        let(:card_10H) { double('card', :value => 10, :suit => :hearts) }
+        let(:card_10S) { double('card', :value => 10, :suit => :spades) }
+        let(:card_9C)  { double('card', :value => 9,  :suit => :clubs) }
+        let(:card_8C)  { double('card', :value => 8,  :suit => :clubs) }
+        let(:card_7C)  { double('card', :value => 7,  :suit => :clubs) }
+        let(:card_7D)  { double('card', :value => 7,  :suit => :diamonds) }
+        let(:card_4C)  { double('card', :value => 4,  :suit => :clubs) }
+
+        xit 'recognizes a hand with a straight flush' do
+            hand.add_card(card_JC)
+            hand.add_card(card_10C)
+            hand.add_card(card_9C)
+            hand.add_card(card_8C)
+            hand.add_card(card_7C)
+            expect(hand.classif_hand).to eq(:straight_flush)
+        end
+
+        it 'recognizes a hand with four of a kind' do
+            hand.add_card(card_10C)
+            hand.add_card(card_10D)
+            hand.add_card(card_10H)
+            hand.add_card(card_10S)
+            hand.add_card(card_4C)
+            expect(hand.classify_hand).to eq(:four_of_a_kind)
+        end
+
+        it 'recognizes a hand with a full house' do
+            hand.add_card(card_10C)
+            hand.add_card(card_10D)
+            hand.add_card(card_10H)
+            hand.add_card(card_7C)
+            hand.add_card(card_7D)
+            expect(hand.classify_hand).to eq(:full_house)
+        end
+
+        it 'recognizes a hand with a flush' do
+            hand.add_card(card_JC)
+            hand.add_card(card_10C)
+            hand.add_card(card_8C)
+            hand.add_card(card_7C)
+            hand.add_card(card_4C)
+            expect(hand.classify_hand).to eq(:flush)
+        end
+
+        it 'recognizes a hand with a straight'
+
+        it 'recognizes a hand with three of a kind' do
+            hand.add_card(card_10C)
+            hand.add_card(card_10D)
+            hand.add_card(card_10H)
+            hand.add_card(card_7C)
+            hand.add_card(card_4C)
+            expect(hand.classify_hand).to eq(:three_of_a_kind)
+        end
+
+        it 'recognizes a hand with two pairs' do
+            hand.add_card(card_10C)
+            hand.add_card(card_10D)
+            hand.add_card(card_7C)
+            hand.add_card(card_7D)
+            hand.add_card(card_4C)
+            expect(hand.classify_hand).to eq(:two_pairs)
+        end
+
+        it 'recognizes a hand with one pair' do
+            hand.add_card(card_10C)
+            hand.add_card(card_10D)
+            hand.add_card(card_8C)
+            hand.add_card(card_7C)
+            hand.add_card(card_4C)
+            expect(hand.classify_hand).to eq(:one_pair)
+        end
+
+        it 'recognizes a hand with nothing (high card)'
+    end
 end
