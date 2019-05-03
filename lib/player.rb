ee953c4ed3b022a0e0e9cb82b24e65298e7d54c4
Renata Cummins <renatacummins@gmail.com>
2019-05-03 11:23:43 -0400

    Add Player#ask_discard

--- lib/player.rb
+++ lib/player.rb
@@ -1,3 +1,5 @@
+require_relative 'hand'
+
 class Player
     attr_reader :name, :pot, :hand
 
@@ -5,5 +7,30 @@ class Player
         @name = name
         @pot = pot
         @hand = Hand.new
+    end
+
+    def ask_discard
+        begin
+            puts "Example: 'AC,2H' = discard ace of clubs and 2 of hearts"
+            puts "#{@name}, enter symbols of 1-3 cards to discard, or 'none': "
+            user_entry = gets.chomp
+            symbols = parse_cards_to_discard(user_entry)
+
+            raise 'you entered more than 3 symbols' if symbols.length > 3
+
+            symbols.each do |symbol|
+                @hand.discard(symbol)
+            end
+        rescue => error
+            puts "Sorry, #{error.message}. Please try again."
+            retry
+        end
+    end
+
+    private
+
+    def parse_cards_to_discard(user_entry)
+        return [] if user_entry.downcase == 'none'
+        return user_entry.upcase.split(',')
     end
 end
