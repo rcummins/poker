bd876f0d8621151671673c416308701b7e27a049
Renata Cummins <renatacummins@gmail.com>
2019-05-03 09:45:22 -0400

    Modify Hand#compare to add support for tie-breaking

--- lib/hand.rb
+++ lib/hand.rb
@@ -39,19 +39,71 @@ class Hand
     end
 
     def compare(other_hand)
-        this_hand_rank = HAND_CLASSIFICATION_RANKS[self.classify_hand]
-        other_hand_rank = HAND_CLASSIFICATION_RANKS[other_hand.classify_hand]
+        this_hand_class = self.classify_hand
+        other_hand_class = other_hand.classify_hand
+
+        this_hand_rank = HAND_CLASSIFICATION_RANKS[this_hand_class]
+        other_hand_rank = HAND_CLASSIFICATION_RANKS[other_hand_class]
 
         return :win if this_hand_rank > other_hand_rank
         return :lose if this_hand_rank < other_hand_rank
 
         # if classifications are the same, look at the card values
-        case this_hand_rank
-        when :straight_flush
+        case this_hand_class
+        when :straight_flush, :straight
+            return :win if self.card_values.max > other_hand.card_values.max
+            return :lose if self.card_values.max < other_hand.card_values.max
+            return :tie
+        when :four_of_a_kind
+            this_hand_value = self.find_X_of_a_kind_values(4).first
+            other_hand_value = other_hand.find_X_of_a_kind_values(4).first
+            return :win if this_hand_value > other_hand_value
+            return :lose
+        when :full_house, :three_of_a_kind
+            this_hand_value = self.find_X_of_a_kind_values(3).first
+            other_hand_value = other_hand.find_X_of_a_kind_values(3).first
+            return :win if this_hand_value > other_hand_value
+            return :lose
+        when :flush, :high_card
+            this_hand_values = self.card_values.sort.reverse
+            other_hand_values = other_hand.card_values.sort.reverse
+            (0..4).each do |ind|
+                return :win if this_hand_values[ind] > other_hand_values[ind]
+                return :lose if this_hand_values[ind] < other_hand_values[ind]
+            end
+            return :tie
+        when :two_pairs
+            this_hand_pairs = self.find_X_of_a_kind_values(2)
+            other_hand_pairs = other_hand.find_X_of_a_kind_values(2)
+            (0..1).each do |ind|
+                return :win if this_hand_pairs[ind] > other_hand_pairs[ind]
+                return :lose if this_hand_pairs[ind] < other_hand_pairs[ind]
+            end
+            this_hand_kicker = self.find_X_of_a_kind_values(1).first
+            other_hand_kicker = other_hand.find_X_of_a_kind_values(1).first
+            return :win if this_hand_kicker > other_hand_kicker
+            return :lose if this_hand_kicker < other_hand_kicker
+            return :tie
+        when :one_pair
+            this_hand_pair_value = self.find_X_of_a_kind_values(2).first
+            other_hand_pair_value = other_hand.find_X_of_a_kind_values(2).first
+            return :win if this_hand_pair_value > other_hand_pair_value
+            return :lose if this_hand_pair_value < other_hand_pair_value
+            this_hand_kickers = self.find_X_of_a_kind_values(1)
+            other_hand_kickers = other_hand.find_X_of_a_kind_values(1)
+            (0..2).each do |i|
+                return :win if this_hand_kickers[i] > other_hand_kickers[i]
+                return :lose if this_hand_kickers[i] < other_hand_kickers[i]
+            end
+            return :tie
         end
     end
 
-    private
+    protected
+
+    def card_values
+        @cards.map(&:value)
+    end
 
     HAND_CLASSIFICATION_RANKS = {
         straight_flush: 9,
@@ -82,7 +134,7 @@ class Hand
     end
 
     def calculate_card_value_diffs
-        values_high_ace = @cards.map(&:value).sort
+        values_high_ace = card_values.sort
 
         values_low_ace = values_high_ace.map do |original_value|
             original_value == 14 ? 1 : original_value
@@ -101,5 +153,13 @@ class Hand
         else
             return diffs_high_ace
         end
+    end
+
+    def find_X_of_a_kind_values(number_in_group)
+        selected_counts = group_values.select do |card_value, count_of_cards|
+            count_of_cards == number_in_group
+        end
+
+        return selected_counts.keys.sort.reverse
     end
 end
--- spec/hand_spec.rb
+++ spec/hand_spec.rb
@@ -17,11 +17,16 @@ describe Hand do
     let(:card_8D)  { double('card', :value => 8,  :suit => :diamonds) }
     let(:card_7C)  { double('card', :value => 7,  :suit => :clubs) }
     let(:card_7D)  { double('card', :value => 7,  :suit => :diamonds) }
+    let(:card_7H)  { double('card', :value => 7,  :suit => :hearts) }
+    let(:card_7S)  { double('card', :value => 7,  :suit => :spades) }
     let(:card_6C)  { double('card', :value => 6,  :suit => :clubs) }
     let(:card_6D)  { double('card', :value => 6,  :suit => :diamonds) }
     let(:card_5C)  { double('card', :value => 5,  :suit => :clubs) }
     let(:card_4C)  { double('card', :value => 4,  :suit => :clubs) }
+    let(:card_4D)  { double('card', :value => 4,  :suit => :diamonds) }
+    let(:card_3C)  { double('card', :value => 3,  :suit => :clubs) }
     let(:card_3D)  { double('card', :value => 3,  :suit => :diamonds) }
+    let(:card_2D)  { double('card', :value => 2,  :suit => :diamonds) }
     let(:card_2H)  { double('card', :value => 2,  :suit => :hearts) }
 
     describe '#initialize' do
@@ -158,7 +163,7 @@ describe Hand do
     end
 
     describe '#compare' do
-        it 'returns win when classification of other hand is lower rank' do
+        it 'returns :win when classification is higher rank than other hand' do
             hand.add_card(card_9C)
             hand.add_card(card_8C)
             hand.add_card(card_7C)
@@ -172,7 +177,7 @@ describe Hand do
             expect(hand.compare(other_hand)).to eq(:win)
         end
 
-        it 'returns lose when classification of other hand is higher rank' do
+        it 'returns :lose when classification is lower rank than other hand' do
             hand.add_card(card_QC)
             hand.add_card(card_JC)
             hand.add_card(card_8C)
@@ -186,8 +191,8 @@ describe Hand do
             expect(hand.compare(other_hand)).to eq(:lose)
         end
 
-        context 'when classification of both hands is the same' do
-            xit 'returns win when straight flush high card is higher' do
+        context 'when classification of both hands is straight flush' do
+            it 'returns :win when high card is higher than other hand' do
                 hand.add_card(card_10D)
                 hand.add_card(card_9D)
                 hand.add_card(card_8D)
@@ -199,6 +204,414 @@ describe Hand do
                 other_hand.add_card(card_6C)
                 other_hand.add_card(card_5C)
                 expect(hand.compare(other_hand)).to eq(:win)
+            end
+
+            it 'returns :lose when high card is lower than other hand' do
+                hand.add_card(card_9C)
+                hand.add_card(card_8C)
+                hand.add_card(card_7C)
+                hand.add_card(card_6C)
+                hand.add_card(card_5C)
+                other_hand.add_card(card_10D)
+                other_hand.add_card(card_9D)
+                other_hand.add_card(card_8D)
+                other_hand.add_card(card_7D)
+                other_hand.add_card(card_6D)
+                expect(hand.compare(other_hand)).to eq(:lose)
+            end
+
+            it 'returns :tie when the hands differ only by suit' do
+                hand.add_card(card_10C)
+                hand.add_card(card_9C)
+                hand.add_card(card_8C)
+                hand.add_card(card_7C)
+                hand.add_card(card_6C)
+                other_hand.add_card(card_10D)
+                other_hand.add_card(card_9D)
+                other_hand.add_card(card_8D)
+                other_hand.add_card(card_7D)
+                other_hand.add_card(card_6D)
+                expect(hand.compare(other_hand)).to eq(:tie)
+            end
+        end
+
+        context 'when classification of both hands is four of a kind' do
+            it 'returns :win when value of 4 cards higher than other hand' do
+                hand.add_card(card_10C)
+                hand.add_card(card_10D)
+                hand.add_card(card_10H)
+                hand.add_card(card_10S)
+                hand.add_card(card_4C)
+                other_hand.add_card(card_7C)
+                other_hand.add_card(card_7D)
+                other_hand.add_card(card_7H)
+                other_hand.add_card(card_7S)
+                other_hand.add_card(card_5C)
+                expect(hand.compare(other_hand)).to eq(:win)
+            end
+        end
+
+        context 'when classification of both hands is full house' do
+            it 'returns :lose when value of 3 cards lower than other hand' do
+                hand.add_card(card_7C)
+                hand.add_card(card_7D)
+                hand.add_card(card_7H)
+                hand.add_card(card_9C)
+                hand.add_card(card_9D)
+                other_hand.add_card(card_10C)
+                other_hand.add_card(card_10D)
+                other_hand.add_card(card_10H)
+                other_hand.add_card(card_8C)
+                other_hand.add_card(card_8D)
+                expect(hand.compare(other_hand)).to eq(:lose)
+            end
+        end
+
+        context 'when classification of both hands is flush' do
+            it 'returns :win when high card is higher than other hand' do
+                hand.add_card(card_JC)
+                hand.add_card(card_9C)
+                hand.add_card(card_7C)
+                hand.add_card(card_5C)
+                hand.add_card(card_3C)
+                other_hand.add_card(card_10D)
+                other_hand.add_card(card_8D)
+                other_hand.add_card(card_6D)
+                other_hand.add_card(card_4D)
+                other_hand.add_card(card_2D)
+                expect(hand.compare(other_hand)).to eq(:win)
+            end
+
+            it 'returns :lose when high cards tied, second card lower' do
+                hand.add_card(card_10C)
+                hand.add_card(card_8C)
+                hand.add_card(card_7C)
+                hand.add_card(card_5C)
+                hand.add_card(card_3C)
+                other_hand.add_card(card_10D)
+                other_hand.add_card(card_9D)
+                other_hand.add_card(card_6D)
+                other_hand.add_card(card_4D)
+                other_hand.add_card(card_2D)
+                expect(hand.compare(other_hand)).to eq(:lose)
+            end
+
+            it 'returns :win when 2 cards tied, 3rd card higher' do
+                hand.add_card(card_10C)
+                hand.add_card(card_8C)
+                hand.add_card(card_7C)
+                hand.add_card(card_5C)
+                hand.add_card(card_3C)
+                other_hand.add_card(card_10D)
+                other_hand.add_card(card_8D)
+                other_hand.add_card(card_6D)
+                other_hand.add_card(card_4D)
+                other_hand.add_card(card_2D)
+                expect(hand.compare(other_hand)).to eq(:win)
+            end
+
+            it 'returns :lose when 3 cards tied, 4th card lower' do
+                hand.add_card(card_10C)
+                hand.add_card(card_8C)
+                hand.add_card(card_7C)
+                hand.add_card(card_5C)
+                hand.add_card(card_3C)
+                other_hand.add_card(card_10D)
+                other_hand.add_card(card_8D)
+                other_hand.add_card(card_7D)
+                other_hand.add_card(card_6D)
+                other_hand.add_card(card_2D)
+                expect(hand.compare(other_hand)).to eq(:lose)
+            end
+
+            it 'returns :win when 4 cards tied, 5th card higher' do
+                hand.add_card(card_10C)
+                hand.add_card(card_8C)
+                hand.add_card(card_7C)
+                hand.add_card(card_4C)
+                hand.add_card(card_3C)
+                other_hand.add_card(card_10D)
+                other_hand.add_card(card_8D)
+                other_hand.add_card(card_7D)
+                other_hand.add_card(card_4D)
+                other_hand.add_card(card_2D)
+                expect(hand.compare(other_hand)).to eq(:win)
+            end
+
+            it 'returns :tie when the hands differ only by suit' do
+                hand.add_card(card_10C)
+                hand.add_card(card_8C)
+                hand.add_card(card_7C)
+                hand.add_card(card_6C)
+                hand.add_card(card_4C)
+                other_hand.add_card(card_10D)
+                other_hand.add_card(card_8D)
+                other_hand.add_card(card_7D)
+                other_hand.add_card(card_6D)
+                other_hand.add_card(card_4D)
+                expect(hand.compare(other_hand)).to eq(:tie)
+            end
+        end
+
+        context 'when classification of both hands is straight' do
+            it 'returns :win when high card is higher than other hand' do
+                hand.add_card(card_JC)
+                hand.add_card(card_10D)
+                hand.add_card(card_9C)
+                hand.add_card(card_8C)
+                hand.add_card(card_7C)
+                other_hand.add_card(card_10C)
+                other_hand.add_card(card_9D)
+                other_hand.add_card(card_8D)
+                other_hand.add_card(card_7H)
+                other_hand.add_card(card_6C)
+                expect(hand.compare(other_hand)).to eq(:win)
+            end
+
+            it 'returns :tie when the hands differ only by suit' do
+                hand.add_card(card_10D)
+                hand.add_card(card_9C)
+                hand.add_card(card_8C)
+                hand.add_card(card_7C)
+                hand.add_card(card_6D)
+                other_hand.add_card(card_10C)
+                other_hand.add_card(card_9D)
+                other_hand.add_card(card_8D)
+                other_hand.add_card(card_7H)
+                other_hand.add_card(card_6C)
+                expect(hand.compare(other_hand)).to eq(:tie)
+            end
+        end
+
+        context 'when classification of both hands is three of a kind' do
+            it 'returns :lose when value of 3 cards lower than other hand' do
+                hand.add_card(card_7C)
+                hand.add_card(card_7D)
+                hand.add_card(card_7H)
+                hand.add_card(card_KC)
+                hand.add_card(card_JC)
+                other_hand.add_card(card_10C)
+                other_hand.add_card(card_10D)
+                other_hand.add_card(card_10H)
+                other_hand.add_card(card_6C)
+                other_hand.add_card(card_4C)
+                expect(hand.compare(other_hand)).to eq(:lose)
+            end
+        end
+
+        context 'when classification of both hands is two pairs' do
+            it 'returns :win when value of 1st pair higher than other hand' do
+                hand.add_card(card_10C)
+                hand.add_card(card_10D)
+                hand.add_card(card_7C)
+                hand.add_card(card_7D)
+                hand.add_card(card_4C)
+                other_hand.add_card(card_9C)
+                other_hand.add_card(card_9D)
+                other_hand.add_card(card_8C)
+                other_hand.add_card(card_8D)
+                other_hand.add_card(card_3C)
+                expect(hand.compare(other_hand)).to eq(:win)
+            end
+
+            it 'returns :lose when 1st pair tied, value of 2nd pair lower' do
+                hand.add_card(card_10C)
+                hand.add_card(card_10D)
+                hand.add_card(card_7C)
+                hand.add_card(card_7D)
+                hand.add_card(card_4C)
+                other_hand.add_card(card_10H)
+                other_hand.add_card(card_10S)
+                other_hand.add_card(card_8C)
+                other_hand.add_card(card_8D)
+                other_hand.add_card(card_3C)
+                expect(hand.compare(other_hand)).to eq(:lose)
+            end
+
+            it 'returns :win when pairs tied, value of last card higher' do
+                hand.add_card(card_10C)
+                hand.add_card(card_10D)
+                hand.add_card(card_7C)
+                hand.add_card(card_7D)
+                hand.add_card(card_4C)
+                other_hand.add_card(card_10H)
+                other_hand.add_card(card_10S)
+                other_hand.add_card(card_7H)
+                other_hand.add_card(card_7S)
+                other_hand.add_card(card_3C)
+                expect(hand.compare(other_hand)).to eq(:win)
+            end
+
+            it 'returns :tie when the hands differ only by suit' do
+                hand.add_card(card_10C)
+                hand.add_card(card_10D)
+                hand.add_card(card_7C)
+                hand.add_card(card_7D)
+                hand.add_card(card_4C)
+                other_hand.add_card(card_10H)
+                other_hand.add_card(card_10S)
+                other_hand.add_card(card_7H)
+                other_hand.add_card(card_7S)
+                other_hand.add_card(card_4D)
+                expect(hand.compare(other_hand)).to eq(:tie)
+            end
+        end
+
+        context 'when classification of both hands is one pair' do
+            it 'returns :lose when value of pair lower than other hand' do
+                hand.add_card(card_9C)
+                hand.add_card(card_9D)
+                hand.add_card(card_7C)
+                hand.add_card(card_5C)
+                hand.add_card(card_3C)
+                other_hand.add_card(card_10C)
+                other_hand.add_card(card_10D)
+                other_hand.add_card(card_6C)
+                other_hand.add_card(card_4C)
+                other_hand.add_card(card_2H)
+                expect(hand.compare(other_hand)).to eq(:lose)
+            end
+
+            it 'returns :win when pair tied, value of 1st kicker higher' do
+                hand.add_card(card_10H)
+                hand.add_card(card_10S)
+                hand.add_card(card_7C)
+                hand.add_card(card_5C)
+                hand.add_card(card_3C)
+                other_hand.add_card(card_10C)
+                other_hand.add_card(card_10D)
+                other_hand.add_card(card_6C)
+                other_hand.add_card(card_4C)
+                other_hand.add_card(card_2H)
+                expect(hand.compare(other_hand)).to eq(:win)
+            end
+
+            it 'returns :lose when 3 cards tied, value of 2nd kicker lower' do
+                hand.add_card(card_10H)
+                hand.add_card(card_10S)
+                hand.add_card(card_7C)
+                hand.add_card(card_5C)
+                hand.add_card(card_3C)
+                other_hand.add_card(card_10C)
+                other_hand.add_card(card_10D)
+                other_hand.add_card(card_7D)
+                other_hand.add_card(card_6C)
+                other_hand.add_card(card_2H)
+                expect(hand.compare(other_hand)).to eq(:lose)
+            end
+
+            it 'returns :win when 4 cards tied, value of last card higher' do
+                hand.add_card(card_10H)
+                hand.add_card(card_10S)
+                hand.add_card(card_7C)
+                hand.add_card(card_6C)
+                hand.add_card(card_3C)
+                other_hand.add_card(card_10C)
+                other_hand.add_card(card_10D)
+                other_hand.add_card(card_7D)
+                other_hand.add_card(card_6D)
+                other_hand.add_card(card_2H)
+                expect(hand.compare(other_hand)).to eq(:win)
+            end
+
+            it 'returns :tie when the hands differ only by suit' do
+                hand.add_card(card_10H)
+                hand.add_card(card_10S)
+                hand.add_card(card_7C)
+                hand.add_card(card_6C)
+                hand.add_card(card_3C)
+                other_hand.add_card(card_10C)
+                other_hand.add_card(card_10D)
+                other_hand.add_card(card_7D)
+                other_hand.add_card(card_6D)
+                other_hand.add_card(card_3D)
+                expect(hand.compare(other_hand)).to eq(:tie)
+            end
+        end
+
+        context 'when classification of both hands is high card' do
+            it 'returns :win when value of 1st card higher than other hand' do
+                hand.add_card(card_KC)
+                hand.add_card(card_6C)
+                hand.add_card(card_4D)
+                hand.add_card(card_3C)
+                hand.add_card(card_2D)
+                other_hand.add_card(card_QC)
+                other_hand.add_card(card_6D)
+                other_hand.add_card(card_4C)
+                other_hand.add_card(card_3D)
+                other_hand.add_card(card_2H)
+                expect(hand.compare(other_hand)).to eq(:win)
+            end
+
+            it 'returns :lose when 1st cards tied, value of 2nd card lower' do
+                hand.add_card(card_10H)
+                hand.add_card(card_6C)
+                hand.add_card(card_4D)
+                hand.add_card(card_3C)
+                hand.add_card(card_2D)
+                other_hand.add_card(card_10S)
+                other_hand.add_card(card_9D)
+                other_hand.add_card(card_4C)
+                other_hand.add_card(card_3D)
+                other_hand.add_card(card_2H)
+                expect(hand.compare(other_hand)).to eq(:lose)
+            end
+
+            it 'returns :win when 2 cards tied, value of 3rd card higher' do
+                hand.add_card(card_10H)
+                hand.add_card(card_9C)
+                hand.add_card(card_8D)
+                hand.add_card(card_3C)
+                hand.add_card(card_2D)
+                other_hand.add_card(card_10S)
+                other_hand.add_card(card_9D)
+                other_hand.add_card(card_4C)
+                other_hand.add_card(card_3D)
+                other_hand.add_card(card_2H)
+                expect(hand.compare(other_hand)).to eq(:win)
+            end
+
+            it 'returns :lose when 3 cards tied, value of 4th card lower' do
+                hand.add_card(card_10H)
+                hand.add_card(card_9C)
+                hand.add_card(card_8D)
+                hand.add_card(card_3C)
+                hand.add_card(card_2D)
+                other_hand.add_card(card_10S)
+                other_hand.add_card(card_9D)
+                other_hand.add_card(card_8C)
+                other_hand.add_card(card_7H)
+                other_hand.add_card(card_2H)
+                expect(hand.compare(other_hand)).to eq(:lose)
+            end
+
+            it 'returns :win when 4 cards tied, value of 5th card higher' do
+                hand.add_card(card_10H)
+                hand.add_card(card_9C)
+                hand.add_card(card_8D)
+                hand.add_card(card_7S)
+                hand.add_card(card_3C)
+                other_hand.add_card(card_10S)
+                other_hand.add_card(card_9D)
+                other_hand.add_card(card_8C)
+                other_hand.add_card(card_7H)
+                other_hand.add_card(card_2H)
+                expect(hand.compare(other_hand)).to eq(:win)
+            end
+
+            it 'returns :tie when the hands differ only by suit' do
+                hand.add_card(card_10H)
+                hand.add_card(card_9C)
+                hand.add_card(card_8D)
+                hand.add_card(card_7S)
+                hand.add_card(card_3C)
+                other_hand.add_card(card_10S)
+                other_hand.add_card(card_9D)
+                other_hand.add_card(card_8C)
+                other_hand.add_card(card_7H)
+                other_hand.add_card(card_3D)
+                expect(hand.compare(other_hand)).to eq(:tie)
             end
         end
     end
